11fa0b0feb9f6c845b850138a9f0a6a2e7a92734
nbbuzzy1 <40919504+nbbuzzy1@users.noreply.github.com>
2020-07-29 17:00:30 -0400

    Set up adopt pets app.

---
+++ src/views/Cats.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1>Cats for Adoption</h1>
+    <b-table striped hover :items="cats">
+			<template v-slot:cell(name)="data">
+				<!-- `data.value` is the value after formatted by the Formatter -->
+				<router-link :to="`/pets/${data.index}`">
+					{{ data.value }}
+				</router-link>
+			</template>
+		</b-table>
+  </div>
+</template>
+
+<script>
+import cats from '@/data/cats'
+
+export default {
+  data () {
+    return {
+      cats
+    }
+  }
+}
+</script>
